1e6d56b5f7de749cf8ed8ed98688bb04dc041ebc
tiphaineP1991 <tiphaine.pellet@essec.edu>
2019-11-01 13:35:54 +0100

    toggle menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-console.log('it works')
+console.log("it works");
+
+const hamburger = document.querySelector("#hamburger");
+const drawer = document.querySelector("#drawer");
+hamburger.addEventListener("click", () => {
+  drawer.classList.toggle("menu-display");
+  console.log("clicked");
+});
